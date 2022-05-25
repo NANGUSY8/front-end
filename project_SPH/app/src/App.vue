c390b18ed8164e11f0c0ba267d0102ec922f28e8
3219005447 <234@234>
2022-05-25 19:32:58 +0800

    完成非路由组件MyHeader、MyFooter

---
+++ project_SPH/app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <MyHeader></MyHeader>
+    <MyFooter></MyFooter>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader/MyHeader.vue'
+import MyFooter from './components/MyFooter/MyFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader, MyFooter
+  }
+}
+</script>
+
+<style>
+</style>
