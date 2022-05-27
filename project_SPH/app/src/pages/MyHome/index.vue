6bef77f2396e29c3d6f9a4c59ef7179104426cd5
3219005447 <234@234>
2022-05-27 11:22:54 +0800

    完成Home首页的静态拆分组件

--- project_SPH/app/src/pages/MyHome/index.vue
+++ project_SPH/app/src/pages/MyHome/index.vue
@@ -1,13 +1,37 @@
 <template>
-  <TypeNav></TypeNav>
+  <div>
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Floor></Floor>
+    <Brand></Brand>
+  </div>
 </template>
 
 <script>
+//引入组件
+import ListContainer from '@/pages/MyHome/ListContainer'
+import TodayRecommend from '@/pages/MyHome/TodayRecommend'
+import Rank from '@/pages/MyHome/Rank'
+import Like from '@/pages/MyHome/Like'
+import Floor from '@/pages/MyHome/Floor'
+import Brand from '@/pages/MyHome/Brand'
+
+
 export default {
-  name: 'MyHome'
+  name: 'MyHome',
+  components: {
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
 </style>
