54526f2b9ab04517659696d968a875c16472bc07
NANGUSY8 <1318092404@qq.com>
2022-09-08 12:25:32 +0800

    完成floor动态展示数据

--- project_SPH/app/src/pages/MyHome/index.vue
+++ project_SPH/app/src/pages/MyHome/index.vue
@@ -1,41 +1,46 @@
 <template>
   <div>
-    <TypeNav/>
-    <ListContainer/>
-    <TodayRecommend/>
-    <Rank/>
-    <Like/>
-    <Floor/>
-    <Floor/>
-    <Brand/>
+    <TypeNav />
+    <ListContainer />
+    <TodayRecommend />
+    <Rank />
+    <Like />
+    <Floor v-for="(floor, index) in floorList" :key="floor.id" :list="floor" />
+    <Brand />
   </div>
 </template>
 
 <script>
 //引入组件
-import ListContainer from '@/pages/MyHome/ListContainer'
-import TodayRecommend from '@/pages/MyHome/TodayRecommend'
-import Rank from '@/pages/MyHome/Rank'
-import Like from '@/pages/MyHome/Like'
-import Floor from '@/pages/MyHome/Floor'
-import Brand from '@/pages/MyHome/Brand'
-
+import ListContainer from "@/pages/MyHome/ListContainer";
+import TodayRecommend from "@/pages/MyHome/TodayRecommend";
+import Rank from "@/pages/MyHome/Rank";
+import Like from "@/pages/MyHome/Like";
+import Floor from "@/pages/MyHome/Floor";
+import Brand from "@/pages/MyHome/Brand";
+import { mapState } from "vuex";
 
 export default {
-  name: 'MyHome',
+  name: "MyHome",
   components: {
     ListContainer,
     TodayRecommend,
     Rank,
     Like,
     Floor,
-    Brand
+    Brand,
   },
-  mounted(){
+  mounted() {
     //通知Vuex从mock接口获取floor的模拟数据
-    this.$store.dispatch('home/getFloorList', 'getFloorList')
-  }
-}
+    this.$store.dispatch("home/getFloorList", "getFloorList");
+  },
+  computed: {
+    //向仓库获取floor数据
+    ...mapState({
+      floorList: (state) => state.home.floorList
+    }),
+  },
+};
 </script>
 
 <style lang="less" scoped>
