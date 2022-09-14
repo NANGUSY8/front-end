46003079e02ece6bf3da32dc1cd2c323a67a625b
NANGUSY8 <1318092404@qq.com>
2022-09-14 23:40:59 +0800

    完成Detail详情页的静态组件

--- project_SPH/app/src/router/index.js
+++ project_SPH/app/src/router/index.js
@@ -12,49 +12,50 @@ import MySearch from '@/pages/MySearch'
 import MyLogin from '@/pages/MyLogin'
 import MyRegister from '@/pages/MyRegister'
 import MyHome from '@/pages/MyHome'
-const originalPush = VueRouter.prototype.push
 
+const originalPush = VueRouter.prototype.push
+//重写push方法
 VueRouter.prototype.push = function push(location) {
-  return originalPush.call(this, location).catch(err => err)
+    return originalPush.call(this, location).catch(err => err)
 }
 
 //创建实例对象，去管理路由规则
 export default new VueRouter({
-    routes:[
+    routes: [
         {
-            path:'/home',
-            component:MyHome,
-            meta:{
+            path: '/home',
+            component: MyHome,
+            meta: {
                 //控制MyFooter组件是否显示
-                show:true
+                show: true
             }
         },
         {
-            path:'/login',
-            component:MyLogin,
-            meta:{
-                show:false
+            path: '/login',
+            component: MyLogin,
+            meta: {
+                show: false
             }
         },
         {
-            name:'search',
-            path:'/search/:keyword?',
-            component:MySearch,
-            meta:{
-                show:true
+            name: 'search',
+            path: '/search/:keyword?',
+            component: MySearch,
+            meta: {
+                show: true
             }
         },
         {
-            path:'/register',
-            component:MyRegister,
-            meta:{
-                show:false
+            path: '/register',
+            component: MyRegister,
+            meta: {
+                show: false
             }
         },
         //重定向,在项目运行起来时,访问/,定向到首页
         {
-            path:'*',
-            redirect:'/home'
+            path: '*',
+            redirect: '/home'
         }
     ]
 })
